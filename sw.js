1fc7cb8c915f96865fe60bf8276119024164e20c
Lamphu19 <sebou.burg@gmail.com>
2026-08-25 15:44:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,47 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+const CACHE_NAME = "traceme-v3";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', () => {});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter(
+              (cacheName) =>
+                cacheName !== CACHE_NAME
+            )
+            .map(
+              (cacheName) =>
+                caches.delete(cacheName)
+            )
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
